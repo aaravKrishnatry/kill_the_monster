a91efdff5f8c9f0cfbf53ddb0dab05af9038c79f
aaravKrishnatry <73398830+aaravKrishnatry@users.noreply.github.com>
2021-08-27 12:23:30 -0400

    Add files via upload

---
+++ block.js
@@ -0,0 +1,22 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+                     
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image=loadImage("monster.png")
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image,0,this.width, this.height);
+        pop();
+      }
+}
